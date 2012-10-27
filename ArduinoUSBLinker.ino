4b9f0940e1ab1e103df7dffdb7483cee2a956b97
c-- <chris@luadev.com>
2012-10-27 15:06:51 -0400

    MultiWii mode seems to be working. Updated documentation and added patch file.

--- ArduinoUSBLinker.ino
+++ ArduinoUSBLinker.ino
@@ -30,17 +30,17 @@ Known issues:
   bytes so it is not an issue at this time.
 
   The default signaling rate over the servo wire is 136µs per bit or about
-  7 kbps which is close to what an actual USB Linker uses. The fastest
-  supported signaling rate is 32µs (~31 kbps).
+  7 kbps which is close to what an actual USB Linker uses.
 
   Note that the default serial port rate is 115200 and this is separate from
   the servo wire signaling rate. Make sure your tools are using the same
   serial port rate. See SERIALRATE below.
   
 Version history:
-  0.5 Remove interrupt code. Some MultiWii boards can't do interrupts on all
-      of their ESC signal pins. This has more overhead which means a slower
-      maximum signaling rate.
+  0.5 Can now be inserted into MultiWii code base.
+  
+      Remove interrupt code. Some MultiWii boards can't do interrupts on all
+      of their ESC signal pins. Maximum speed around 31 kbps (32µs).
       
       Add runtime configuration commands. The signal pin and bit rate are now
       selectable at runtime. See the README for more information.
@@ -64,8 +64,13 @@ Version history:
 
 ///////////////////////////////////////////////////////////////////////////////
 
-// Serial port
+#if defined(MINTHROTTLE)
+#define MULTIWII
+#define AUL_SERIALRATE 19200
+#else
 #define AUL_SERIALRATE 115200
+#endif
+
 #define AUL_SERIALTIMEOUT (F_CPU / (AUL_SERIALRATE >> 4))
 
 // Calculates ticks from microseconds
@@ -78,6 +83,7 @@ Version history:
 
 #define AUL_MIN_BITTIME 8
 #define AUL_MAX_BITTIME 136
+#define AUL_DEFAULT_BITTIME 136
 
 #define AUL_PININPUT  ((*g_signalDDR)  &= ~(1 << g_signalPinPortNum))
 #define AUL_PINOUTPUT ((*g_signalDDR)  |= (1 << g_signalPinPortNum))
@@ -91,7 +97,7 @@ Version history:
 ///////////////////////////////////////////////////////////////////////////////
 
 // Approximate microseconds for each bit when sending
-static uint16_t g_bitTimeSend = AUL_MICROS(AUL_MAX_BITTIME);
+static uint16_t g_bitTimeSend = AUL_MICROS(AUL_DEFAULT_BITTIME);
 static uint16_t g_bitTimeSendLong = g_bitTimeSend >> 1;
 static uint16_t g_bitTimeSendShort = g_bitTimeSend >> 2;
 
@@ -104,8 +110,134 @@ static volatile uint8_t* g_signalPIN;
 static int8_t g_signalPinPortNum, g_signalPinNum;
 
 ///////////////////////////////////////////////////////////////////////////////
+// Serial port.  MultiWii helpers.
+///////////////////////////////////////////////////////////////////////////////
+
+#if !defined(MULTIWII)
+
+#define AUL_SerialInit(x) Serial.begin(AUL_SERIALRATE)
+#define AUL_SerialAvailable() Serial.available()
+#define AUL_SerialRead() Serial.read()
+#define AUL_SerialWrite(x) Serial.write(x)
+#define AUL_SerialWriteBuf(x,y) Serial.write(x,y)
+#define AUL_SerialWriteStr(x) Serial.write((const char*)x)
+
+#else // MULTIWII
+
+// Tried to use the MultiWii serial port code but it was even worse than this
+// spin-loop method as far as serial errors.
+
+static volatile uint8_t* g_serialUCSRA;
+static volatile uint8_t* g_serialUDR;
+static uint8_t g_serialRXC, g_serialUDRE;
+
+static void AUL_SerialInit(uint8_t port)
+{
+  #define BAUD AUL_SERIALRATE
+  #include <util/setbaud.h>
+
+  #if USE_2X
+    #define AUL_INIT2X(x) UCSR##x##A |= (1 << U2X0)
+  #else 
+    #define AUL_INIT2X(x) UCSR##x##A &= ~(1 << U2X##x);
+  #endif // USE_2X
+  
+  #define AUL_INIT_PORT(x) \
+    UBRR##x##H = UBRRH_VALUE; \
+    UBRR##x##L = UBRRL_VALUE; \
+    AUL_INIT2X(x); \ 
+    UCSR##x##C = (1 << UCSZ##x##1) | (1 << UCSZ##x##0); \
+    UCSR##x##B = (1 << RXEN##x) | (1 << TXEN##x); \
+    g_serialUCSRA = &UCSR##x##A; \
+    g_serialUDR = &UDR##x; \
+    g_serialRXC = (1 << RXC##x); \
+    g_serialUDRE = (1 << UDRE##x);
+  
+  switch (port)
+  {
+  case 0:
+    AUL_INIT_PORT(0)
+    break;
+  #if defined(UBRR1H)
+  case 1:
+    AUL_INIT_PORT(1)
+    break;
+  #endif
+  #if defined(UBRR2H)
+  case 2:
+    AUL_INIT_PORT(2)
+    break;
+  #endif
+  #if defined(UBRR3H)
+  case 3:
+    AUL_INIT_PORT(3)
+    break;
+  #endif
+  #if defined(UBRR4H)
+  case 4:
+    AUL_INIT_PORT(4)
+    break;
+  #endif
+  default:
+    break;
+  }
+}
+
+#define AUL_SerialAvailable() ((*g_serialUCSRA) & g_serialRXC)
+
+#define AUL_SerialRead() (*g_serialUDR)
+
+#define AUL_SerialWrite(x) { while (!((*g_serialUCSRA) & g_serialUDRE)); (*g_serialUDR) = (x); }
+
+static void AUL_SerialWriteBuf(const uint8_t* b, uint16_t len)
+{
+  uint16_t i;
+  for (i = 0; i < len; i++)
+    AUL_SerialWrite(b[i]);
+}
+
+static void AUL_SerialWriteStr(const char* b)
+{
+  uint16_t i;
+  for (i = 0; b[i] != '\0'; i++)
+    AUL_SerialWrite(b[i]);
+}
+
+#endif // MULTIWII
+
+///////////////////////////////////////////////////////////////////////////////
 // Signal pin
 ///////////////////////////////////////////////////////////////////////////////
+
+// Clear all timers and PWM settings
+static void DisableAllTimers()
+{
+  #define AUL_RESET_PORT(x) \
+    TCCR##x##B = 0; \
+    TCCR##x##A = 0;
+  
+  #if defined(TCCR0B)
+    AUL_RESET_PORT(0)
+  #endif
+  #if defined(TCCR1B)
+    AUL_RESET_PORT(1)
+  #endif
+  #if defined(TCCR2B)
+    AUL_RESET_PORT(2)
+  #endif
+  #if defined(TCCR3B)
+    AUL_RESET_PORT(3)
+  #endif
+  #if defined(TCCR4B)
+    AUL_RESET_PORT(4)
+  #endif
+  #if defined(TCCR5B)
+    AUL_RESET_PORT(5)
+  #endif
+  #if defined(TCCR6B)
+    AUL_RESET_PORT(6)
+  #endif
+}
 
 static void SignalPinStatus(char* buf)
 {
@@ -119,6 +251,12 @@ static void SignalPinStatus(char* buf)
   char* pos = buf;
   int8_t pincnt = 0;
 
+  *pos++ = 'P';
+  *pos++ = 'I';
+  *pos++ = 'N';
+  *pos++ = 'S';
+  *pos++ = ':';
+  
   #if defined(PORTB)
     AUL_WRITE_PORT_INFO(B)
   #endif
@@ -222,13 +360,13 @@ finished:
 ///////////////////////////////////////////////////////////////////////////////
 // SENDING on signal pin
 ///////////////////////////////////////////////////////////////////////////////
-static inline void delayTicks(uint16_t count)
+static void delayTicks(uint16_t count)
 {
   TCNT1 = 0;
   while (TCNT1 < count);
 }
 
-static inline void Send1()
+static void Send1()
 {
   AUL_PINHIGH;
   AUL_LONGBITDELAY;
@@ -236,7 +374,7 @@ static inline void Send1()
   AUL_LONGBITDELAY;
 }
 
-static inline void Send0()
+static void Send0()
 {
   AUL_PINHIGH;
   AUL_SHORTBITDELAY;
@@ -248,7 +386,7 @@ static inline void Send0()
   AUL_SHORTBITDELAY;
 }
 
-static inline void SendByte(uint8_t b)
+static void SendByte(uint8_t b)
 {
   (b & B00000001) ? Send1() : Send0();
   (b & B00000010) ? Send1() : Send0();
@@ -265,7 +403,7 @@ static inline void SendByte(uint8_t b)
 ///////////////////////////////////////////////////////////////////////////////
 
 // NOTE: This has a maximum wait time of about 2000µs before it overflows
-static inline int16_t WaitPinHighLow(uint16_t timeout)
+static int16_t WaitPinHighLow(uint16_t timeout)
 {
   while (AUL_PINREAD)
     if (TCNT1 > timeout)
@@ -281,7 +419,7 @@ static inline int16_t WaitPinHighLow(uint16_t timeout)
   return last;
 }
 
-static inline int8_t ReadBit()
+static int8_t ReadBit()
 {
   int16_t timer = WaitPinHighLow(g_bitTime << 1);
 
@@ -298,39 +436,23 @@ static inline int8_t ReadBit()
 
 static int8_t ReadLeader()
 {
+  uint8_t i;
+  
   // Skip the first few to let things stabilize
-  if (WaitPinHighLow(AUL_LONGWAIT) < 0) return -1;
-  if (WaitPinHighLow(AUL_LONGWAIT) < 0) return -2;
-  if (WaitPinHighLow(AUL_LONGWAIT) < 0) return -3;
-  if (WaitPinHighLow(AUL_LONGWAIT) < 0) return -4;
-  if (WaitPinHighLow(AUL_LONGWAIT) < 0) return -5;
-  if (WaitPinHighLow(AUL_LONGWAIT) < 0) return -6;
-  if (WaitPinHighLow(AUL_LONGWAIT) < 0) return -7;
-  if (WaitPinHighLow(AUL_LONGWAIT) < 0) return -8;
-  if (WaitPinHighLow(AUL_LONGWAIT) < 0) return -9;
+  for (i = 0; i < 9; i++)
+    if (WaitPinHighLow(AUL_LONGWAIT) < 0) return -1;
 
   int16_t timer;
   g_bitTime = 0;
 
   // Average the next 8 to get our bit timing
   // NOTE: this has a window of around 4000µs before we overflow
-  if ((timer = WaitPinHighLow(AUL_LONGWAIT)) < 0) return -10;
-  g_bitTime += timer;
-  if ((timer = WaitPinHighLow(AUL_LONGWAIT)) < 0) return -11;
-  g_bitTime += timer;
-  if ((timer = WaitPinHighLow(AUL_LONGWAIT)) < 0) return -12;
-  g_bitTime += timer;
-  if ((timer = WaitPinHighLow(AUL_LONGWAIT)) < 0) return -13;
-  g_bitTime += timer;
-  if ((timer = WaitPinHighLow(AUL_LONGWAIT)) < 0) return -14;
-  g_bitTime += timer;
-  if ((timer = WaitPinHighLow(AUL_LONGWAIT)) < 0) return -15;
-  g_bitTime += timer;
-  if ((timer = WaitPinHighLow(AUL_LONGWAIT)) < 0) return -16;
-  g_bitTime += timer;
-  if ((timer = WaitPinHighLow(AUL_LONGWAIT)) < 0) return -17;
-  g_bitTime += timer;
-
+  for (i = 0; i < 8; i++)
+  {
+    if ((timer = WaitPinHighLow(AUL_LONGWAIT)) < 0) return -1;
+    g_bitTime += timer;
+  }
+  
   g_bitTime >>= 3;
   g_shortBitTime = (g_bitTime >> 1) + (g_bitTime >> 2);
 
@@ -348,104 +470,74 @@ static int8_t ReadLeader()
   return 0;
 }
 
-static inline int16_t ReadByte()
+static int16_t ReadByte()
 {
   int16_t b;
-  int8_t b2;
+  int8_t b2, i;
 
-  if ((b = ReadBit()) < 0) return -1;
-
-  if ((b2 = ReadBit()) < 0) return -1;
-  b |= b2 << 1;
-
-  if ((b2 = ReadBit()) < 0) return -1;
-  b |= b2 << 2;
-
-  if ((b2 = ReadBit()) < 0) return -1;
-  b |= b2 << 3;
-
-  if ((b2 = ReadBit()) < 0) return -1;
-  b |= b2 << 4;
-
-  if ((b2 = ReadBit()) < 0) return -1;
-  b |= b2 << 5;
-
-  if ((b2 = ReadBit()) < 0) return -1;
-  b |= b2 << 6;
-
-  if ((b2 = ReadBit()) < 0) return -1;
-  b |= b2 << 7;
-
+  for (i = 0; i < 8; i++)
+  {
+    if ((b2 = ReadBit()) < 0) return -1;
+    b |= b2 << i;
+  }
+  
   return b;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// Arduino
+// Main
 ///////////////////////////////////////////////////////////////////////////////
 
-void setup()
+void AUL_loop(uint8_t port)
 {
-  Serial.begin(AUL_SERIALRATE);
-
+  // Disable interrupts and timers
   cli();
-
+  DisableAllTimers();
+  AUL_SerialInit(port);
+  
+  #if !defined(MULTIWII)
+    sei();
+  #endif
+  
   // Set up timer1 to count ticks  
   TCCR1B = (1 << CS10);
   TCCR1A = 0;
 
   SignalPinInit(18); // INT0
 
-  sei();
-}
-
-void loop()
-{
   // The buffer always has the leader at the start
   uint8_t buf[1024] = { 0xFF, 0xFF, 0x7F };
-  int16_t buflen, b, i;
+  int16_t buflen, i;
 
   while (1)
   {
-    b = Serial.read();
-    if (b >= 0)
+    if (AUL_SerialAvailable())
     {
       buflen = 3;
-      buf[buflen++] = b;
+      buf[buflen++] = AUL_SerialRead();
   
       // Buffer data until the serial timeout
       TCNT1 = 0;
       do {
-         b = Serial.read();
-         if (b >= 0)
+         if (AUL_SerialAvailable())
          {
-           buf[buflen++] = b;
+           buf[buflen++] = AUL_SerialRead();
            TCNT1 = 0;
          }
       } while (TCNT1 < AUL_SERIALTIMEOUT);
-
-      if (buf[3] == '$' && buf[4] == '<' && buf[5] == ':')
+      
+      if (buf[3] == '$' && buf[4] == 'M' && buf[5] == '<')
       {
+        buf[buflen] = '\0';
+        
         switch(buf[6])
         {
-        case '-': { // STATUS
-          char* pos = (char*)&buf[3];
-          *pos++ = 'P';
-          itoa(g_signalPinNum, pos, 10);
-          pos = strchr(pos, '\0');
-          *pos++ = ':';
-          *pos++ = 'B';
-          itoa(g_bitTimeSend / (F_CPU / 1000000), pos, 10);
-          pos = strchr(pos, '\0');
-          *pos++ = ':';
-          
-          SignalPinStatus(pos);
-          
-          Serial.write((const char*)&buf[3]);
-          break; }
         case 'B': { // BITTIME
           g_bitTimeSend = atoi((const char*)&buf[7]);
   
-          if (g_bitTimeSend < AUL_MIN_BITTIME || g_bitTimeSend > AUL_MAX_BITTIME)
+          if (g_bitTimeSend < AUL_MIN_BITTIME)
+            g_bitTimeSend = AUL_MIN_BITTIME;
+          else if (g_bitTimeSend > AUL_MAX_BITTIME)
             g_bitTimeSend = AUL_MAX_BITTIME;
   
           g_bitTimeSend = AUL_MICROS(g_bitTimeSend);
@@ -459,12 +551,28 @@ void loop()
           AUL_PINHIGH;
           SignalPinInit(oldpin);
           break; }
-        case 'S': // SELECT
+        case 'P': // SELECT PORT
           SignalPinInit(atoi((const char*)&buf[7]));
           break;
         default:
           break;
         }
+        
+        // Send status afterwards
+        char* pos = (char*)&buf[3];
+        *pos++ = 'P';
+        itoa(g_signalPinNum, pos, 10);
+        pos = strchr(pos, '\0');
+        *pos++ = ':';
+        *pos++ = 'B';
+        itoa(g_bitTimeSend / (F_CPU / 1000000), pos, 10);
+        pos = strchr(pos, '\0');
+        *pos++ = ':';
+        
+        SignalPinStatus(pos);
+        
+        AUL_SerialWriteStr((const char*)&buf[3]);
+        AUL_SerialWrite('\n');
       }
       else
       {
@@ -488,19 +596,24 @@ void loop()
       // Buffer data from signal pin then write to serial port
       if (ReadLeader() == 0)
       {
+        int16_t b;
+        
         while ((b = ReadByte()) >= 0)
           buf[buflen++] = b;
   
-        Serial.write(&buf[3], buflen - 3);
+        AUL_SerialWriteBuf(&buf[3], buflen - 3);
       }
     }
   }
 }
 
+#if !defined(MULTIWII)
+
 int main(int argc, char* argv[])
 {
-  setup();
-  while (1) loop();
+  AUL_loop(0);
   return 0;
 }
+
+#endif // MULTIWII
 
--- README.md
+++ README.md
@@ -21,3 +21,64 @@ To use:
 
 Please look at the top of the source file for notes and more information.
 
+Advanced usage:
+
+ * The ArduinoUSBLinker supports run-time configuration of which pin to use for
+   signaling and at what bit rate to run at. These options are configured by
+   using text commands over the serial port.
+
+   The command format is similar to but not the same as the MultiWii serial
+   protocol. All messages start with "$M&lt;" followed by a command character
+   and its parameter(s).
+
+   Supported commands are:
+   $M&lt;In : Initializes pin number n by turning on its pull-up resistor.
+              This will hold the ESC in bootloader mode. All ESC pins should
+              be initialized first to prevent accidental motor power-up.
+              Example: "$M&lt;I1" will initialize pin 1 (PB1).
+
+   $M&lt;Pn : Select pin number n. This also set the pull-up resistor for the
+              pin. The currently selected pin will be used for signaling.
+              Example: "$M&lt;P18" will select pin 18 (PD3/INT1).
+
+   $M&lt;Bn : Sets the bit rate in microseconds.
+              Example: "$M&lt;B32" will set a 32us signaling rate.
+
+   All commands, including invalid commands will return the current settings.
+   The settings are followed by a list of all the ports and the starting pin
+   number for each.
+
+   Example status line:
+   P18:B136:PINS:B0:C8:D16:
+
+   Pin 18 is selected, bit rate is 136us and there are 3 ports:
+   B starts with pin 0.
+   C starts with pin 8.
+   D starts with pin 16.
+
+   This idicates pin 0-7 is PB0..7, pin 8-15 is PC0..7, pin 16-24 is PD0..7,
+   and so on. These are the pin numbers used with the above commands.
+
+ * This sketch can be integrated with MultiWii. Copy the "ArduinoUSBLinker.ino"
+   file in to the MultiWii sketch folder and rename it as "ArduinoUSBLinker.h".
+   Apply the supplied patch "Serial.patch" to the "Serial.ino" file.
+
+   The ArduinoUSBLinker code adds about 3K to the firmware size and depending
+   on which MultiWii options are configured there may not be enough room for
+   everything.
+   
+   To enter the ArduinoUSBLinker mode a MultiWii command 211 must be sent using
+   the MultiWii serial protocol. This is a binary string of the following
+   6 characters (in hex): 24  4D  3C  00        D3    D3
+                          '$' 'M' '<' <datalen> <211> <checksum>
+
+   Note the "211" command is subject to change and is not part of the official
+   MultiWii code base.
+
+   Once in ArduinoUSBLinker mode it behaves as normal. However, the default
+   baud rate is 19200 and it is not recommended to change this to a faster
+   speed as higher rates are unreliable when running in MultiWii mode. Note
+   that the baud rate will most likely change when entering ArduinoUSBLinker
+   mode from normal MultiWii mode.
+
+   To exit ArduinoUSBLinker mode reset the device.
